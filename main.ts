93c3bd7216bb9832aafb51395728048ed58e2d66
m3211n <130044007+m3211n@users.noreply.github.com>
2023-04-10 02:04:33 +0200

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    let Y = 0
     for (let Y = 0; Y <= maxIndex; Y++) {
         list = sampleCol(Y)
         list = pushBackward(list)
+        replaceCol(Y, list)
     }
-    replaceCol(Y, list)
+    add2()
+    updateCells()
 })
 function replaceCol (i: number, src: number[]) {
     for (let Y = 0; Y <= maxIndex; Y++) {
@@ -43,6 +44,8 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         list = pushBackward(list)
         replaceRow(Y, list)
     }
+    add2()
+    updateCells()
 })
 function pushBackward (_set: number[]) {
     tmpSet = _set
@@ -79,14 +82,39 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         list = pushForward(list)
         replaceRow(Y, list)
     }
+    add2()
+    updateCells()
 })
+function add2 () {
+    Added2 = false
+    randomIndex = randint(0, mainListDimension ** 2 - 1)
+    while (!(Added2)) {
+        if (mainList[randomIndex] == 0) {
+            mainList[randomIndex] = 2
+            Added2 = true
+        }
+    }
+}
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     for (let Y = 0; Y <= maxIndex; Y++) {
         list = sampleCol(Y)
         list = pushForward(list)
         replaceCol(Y, list)
     }
+    add2()
+    updateCells()
 })
+function updateCells () {
+    for (let Y0 = 0; Y0 <= maxIndex; Y0++) {
+        for (let X0 = 0; X0 <= maxIndex; X0++) {
+            textSprite = textsprite.create(convertToText(mainList[Y0 * mainListDimension + X0]), 1, 15)
+            tiles.placeOnTile(textSprite, tiles.getTileLocation(X0, Y0))
+        }
+    }
+}
+let textSprite: TextSprite = null
+let randomIndex = 0
+let Added2 = false
 let cell = 0
 let zerosToAdd = 0
 let Merged = false
@@ -98,8 +126,12 @@ let mainList: number[] = []
 let maxIndex = 0
 let mainListDimension = 0
 scene.setBackgroundColor(12)
+tiles.setCurrentTilemap(tilemap`level1`)
 mainListDimension = 4
 maxIndex = mainListDimension - 1
 mainList = []
 createList()
+add2()
+add2()
+updateCells()
 console.log(mainList)
---
+++ tilemap.g.ts
@@ -0,0 +1,31 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`0a0008000202020201010101010102020202010101010101020202020101010101010202020201010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath5,sprites.castle.tileGrass1], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
